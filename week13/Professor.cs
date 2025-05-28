d815832496f4edb482ecf8fd6faf93dd79161922
rennOobayashi <devforplayer@gmail.com>
2025-05-28 12:24:59 +0900

    Add files via upload

--- week13/Professor.cs
+++ week13/Professor.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Week09Homework
 {
-    class Professor : Member
+    class Professor : Member, IFile
     {
         //public string DepartmentCode { get; set; }
 
